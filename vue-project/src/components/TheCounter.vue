00bb086adc5a3e3b3d506c9cac2793c2dd45a102
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-03-28 12:21:11 +0100

    Exercise six done

--- vue-project/src/components/TheCounter.vue
+++ vue-project/src/components/TheCounter.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const counter=ref<number>(0)
-
-
+const counter = ref<number>(0);
 </script>
+
 <template>
-
-    <p>Número: {{ counter }}</p>
-    <button @click="counter++">Incrementar</button>
-    <button @click="counter--" v-if="counter>0">Decrementar</button>
-    <p v-else>No se permite decrementar a números negativos</p>
-  
+  <p>Número: {{ counter }}</p>
+  <button @click="counter++" v-if="counter < 10">Incrementar</button>
+  <p v-if="counter >= 10">No se permite incrementar a números mayores que 10</p>
+  <button @click="counter--" v-if="counter > 0">Decrementar</button>
+  <p v-if="counter <= 0">No se permite decrementar a números negativos</p>
 </template>
-<style scoped>
 
+<style scoped>
 </style>
