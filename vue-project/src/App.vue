3b003230f7cc5bea7e5b7ba41527687b05ea1d38
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-03-28 09:28:49 +0100

    Second exercise done

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <TheTitle />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheTitle from './components/TheTitle.vue';
+
+</script>
+
+<style>
+
+</style>
---
+++ vue-project/src/components/TheTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2 class="title">Título estático</h2>
+  </template>
+  
+  <style scoped>
+  .title {
+    font-size: 2rem;
+    color: var(--primary-color);
+    text-align: center;
+    margin: 20px 0;
+  }
+  </style>
