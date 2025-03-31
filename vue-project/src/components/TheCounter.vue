6f99feba7afbfdbf22266195944a5a8e20e6c84f
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-03-31 12:18:45 +0200

    Exercise fourteen done

--- vue-project/src/components/TheCounter.vue
+++ vue-project/src/components/TheCounter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 const counter = ref<number>(0);
 const activeClass = 'active';
@@ -7,11 +7,23 @@ const errorClass = 'error';
 
 const doubledCounter = computed(() => counter.value * 2);
 
+const message = ref<string>('');
+
+watch(counter, (newCounter) => {
+  if (newCounter === 0) {
+    message.value = 'Estás en el valor mínimo';
+  } else if (newCounter === 10) {
+    message.value = 'Estás en el valor máximo';
+  } else {
+    message.value = 'Estás en los parámetros adecuados';
+  }
+});
 </script>
 
 <template>
   <p>Número: {{ counter }}</p>
   <p>Doble del número: {{ doubledCounter }}</p>
+  <p>{{ message }}</p>
   <button @click="counter++" v-if="counter < 10">Incrementar</button>
   <p v-if="counter >= 10" :class="counter >= 10 ? activeClass : errorClass">No se permite incrementar a números mayores que 10</p>
   <button @click="counter--" v-if="counter > 0">Decrementar</button>
