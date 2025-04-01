83eae24dfd4bc7f61d04f1b139ca54c6758e311a
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-04-01 10:14:47 +0200

    Exercise three done

--- nuxt-project/pages/home.vue
+++ nuxt-project/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>Bienvenido a la página principal</h2>
-     <NuxtLink to="/">Home</NuxtLink>
+     <NuxtLink to="/home">Home</NuxtLink>
      <NuxtLink to="/catalogo">Catálogo</NuxtLink>
   </div>
 </template>
---
+++ nuxt-project/pages/productos.vue
@@ -0,0 +1,14 @@
+<script>
+import defineNuxtRouteMiddleware from '@/middleware/redirectIndex'
+definePageMeta({
+    middleware:[defineNuxtRouteMiddleware]
+})
+
+console.log('He entrado!!!')
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
