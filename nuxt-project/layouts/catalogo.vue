10afee82a0e6dc44195f068c1d68c49f58e4c742
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-04-01 09:51:42 +0200

    Exercise two done

---
+++ nuxt-project/layouts/catalogo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <nav>
+      <h2>Menú</h2>
+    </nav>
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped>
+nav {
+  padding: 10px;
+  background-color: #f0f0f0;
+  text-align: center;
+}
+
+h2 {
+  margin: 0;
+}
+</style>
---
+++ nuxt-project/layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <nav>
+      <h2>Menú</h2>
+    </nav>
+    <NuxtPage />
+    <footer>
+      <h2>Footer</h2>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+nav, footer {
+  padding: 10px;
+  background-color: #f0f0f0;
+  text-align: center;
+}
+
+h2 {
+  margin: 0;
+}
+</style>
